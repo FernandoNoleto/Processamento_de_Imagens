3a7ff135d3e3d5223d97ece0405ec80a07e418b4
FernandoNoleto <fernandoleto@hotmail.com>
2018-08-16 21:30:28 -0300

    conversao para escala de cinza ok

--- interpolacao_img.py
+++ interpolacao_img.py
@@ -1,16 +1,63 @@
 from PIL import Image, ImageFilter
+import numpy as np
 
-im = Image.open('BolsoSimpson.jpg') # Can be many different formats.
-pix = im.load()
-x = 1
-y = 1
-value = 0
-print im.size  # Get the width and hight of the image for iterating over
-print pix[x,y]  # Get the RGBA Value of the a pixel of an image
+def abrir_imagem(nome_img):
+    img = Image.open(nome_img) # Can be many different formats.
+    return img
 
-for i in range(im.width):
-    for j in range(im.width):
-        pix[i,j] = (2, 252, 6)
+def converter_para_escala_de_cinza(img):
+    img = img.convert('L') # Convert image to monochrome
+    return img
 
-# pix[x,y] = value  # Set the RGBA Value of the image (tuple)
-im.save('saida.png')  # Save the modified pixels as .png
+def interpolacao_vizinho_mais_proximo(img, width, height):
+    nova_matriz = []
+    x = 0
+    y = 0
+
+    for i in range(width/2):
+        linha = []
+        for j in range(height/2):
+            linha.append(img[y][x])
+            x += 2
+        x = 0
+        y += 2
+        nova_matriz.append(linha)
+    
+    return nova_matriz
+
+def imprimir_matriz(matriz):
+
+    print(np.matrix(matriz))
+
+
+def main():
+    img = abrir_imagem('BolsoSimpson.jpg')
+    pix = img.load()
+    x = 1
+    y = 1
+    # value = 0
+    print('size: {}'.format(img.size))  # Get the width and height of the image for iterating over
+    print('pix: {}'.format(pix[x,y]))  # Get the RGBA Value of the a pixel of an image
+
+
+    img = converter_para_escala_de_cinza(img)
+    
+
+    # for i in range(0, img.height, 2):
+    #     for j in range(0, img.width, 2):
+    #         pix[i,j] = 0
+            
+    # pix[x,y] = value  # Set the RGBA Value of the image (tuple)
+    img.save('grayscale.png')  # Save the modified pixels as .png
+    # img.show()
+
+    # img = Image.open('grayscale.png')
+    # pix = img.load()
+    # print('pix: {}'.format(pix[x,y]))
+
+    nova_matriz = interpolacao_vizinho_mais_proximo(img, img.width, img.height)
+    print('Matriz reduzida (Vizinho mais proximo): ')
+    imprimir_matriz(nova_matriz)
+
+if __name__ == '__main__':
+    main()
